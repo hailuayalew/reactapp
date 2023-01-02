20075f5b9e168c7ed57d5d68012f473d144f9793
hailu ayalew <119626036+hailuayalew@users.noreply.github.com>
2023-01-02 18:10:30 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import { View } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-function App() {
+export default function App() {
   return (
-    <View><Text>App</Text></View>
-  )
+    <View style={styles.container}>
+      <Text>helll world</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
